fb352201799195b3dc2e043abb35984a45143bb9
Tech-DS <bta34280@gmail.com>
2020-12-25 10:53:03 +0000

    null修正

--- db/migrate/20201211163320_create_posts.rb
+++ db/migrate/20201211163320_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
     create_table :posts do |t|
       t.integer :senior_id
       t.text :body,       null: false
-      t.string :image_id, null: false
+      t.string :image_id, null: true
       t.timestamps
     end
   end
